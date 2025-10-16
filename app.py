ca096ff58fed5e02d70d80c4d86efb798b37f155
Rawan Elhady <rawan@gmail.com>
2025-10-16 14:59:07 +0300

    Add initial Flask application setup with a basic route

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
 print("hil")
-from flask import Flask
+from flask import Flask, jsonify, render_template
 #app da object mn Flask class
 # w klmt __name__ btshow the particular script was invoked
 app = Flask(__name__)
+
+
+JOBS=[
+    {
+        'id':1, 
+        'title':'Data Analyst',
+        'location':'Bangalore, India',
+        'salary':'Rs. 10,00,000'
+    },
+    {
+        'id':2, 
+        'title':'Data Scientist',
+        'location':'Delhi, India',
+        'salary':'Rs. 20,00,000'
+    },
+    {
+        'id':3, 
+        'title':'frontend Engineer',
+        'location':'Berlin, Germany',
+        'salary':'€60,000'
+    },
+    {
+        'id':4, 
+        'title':'Backend Engineer',
+        'location':'San Francisco, USA',
+        'salary':'$1,00,000'
+    },
+
+]
 @app.route('/')
 def hello_world():
-    return 'Hellxo,d!'
+    return render_template('home.html',jobs=JOBS)
 
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
